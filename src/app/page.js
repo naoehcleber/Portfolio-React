d2a2fd2d080e1d1581000b1c21fe0d1212afc0d2
naoehcleber <joao.vr.fernandes@gmail.com>
2025-09-09 18:35:30 -0300

    placeholders

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,35 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
+import { FaGithub, FaLinkedin } from "react-icons/fa6";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="main">
+      <div className={styles.personal}>
+      {/* foto + pequena descrição */}
+      <Image src="/placeholder.png" width={300} height={300} alt="João Victor Rocha Fernandes"></Image>
+      <h1>João Victor Rocha Fernandes</h1>
+      <p>Estudante de ciência da computação na UNICAP. Desenvolvedor Fullstack. Interesse em Redes de computadores, DevOps e Infraestrutura.</p>
+      <Link href={"https://github.com/naoehcleber"}> <FaGithub /> </Link>
+      <Link href={"www.linkedin.com/in/joão-victor-rocha-980a12290"}> <FaLinkedin /> </Link>
+      </div>
+      <div className={styles.professional}>
+        {/* curriculo profissional */}
+        <h2> Formações Acadêmicas </h2>
+        <p>Bacharelado em Ciência da Computação — 2023 - Atual</p>
+        <h2> Experiências Profissionais </h2>
+        <p>Analista Júnior de NOC — Accenture — Julho de 2024 - Outubro de 2024 </p>
+        <h2> Conhecimentos Técnicos </h2>
+        <p>
+          Linguagens de programação Python, JavaScript, Java, C/C++ <br />
+          Banco de dados relacionais (MySQL, MariaDB, Postgres) e não relacionais (MongoDB) <br/>
+          Framewoks React, Django, Express, SpringBoot <br/>
+        </p>
+        <h2> Cursos </h2>
+        <p>Curso de desenvolvimento Front-End — Senac</p>
+      </div>
     </div>
   );
 }
+
